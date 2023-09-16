529e4cca2bccd4302b3362c6dfd9cdf432814b97
lmbanegas <lbanegas93@gmail.com>
2023-09-16 16:35:22 -0300

    .

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 app.use(express.static('public'));
+const publicPath = path.join(__dirname,'public');
+
 
 
 app.get('/', (req, res) => {
