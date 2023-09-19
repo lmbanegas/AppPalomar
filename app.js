bd96babf37ae3ff8f5a0dfad08667e771df97826
lmbanegas <lbanegas93@gmail.com>
2023-09-19 20:52:34 -0300

    575RT

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const path = require('path');
+const puerto = process.env.PORT || 3000;
+
 
 const app = express();
 app.use(express.static('public'));
 const publicPath = path.join(__dirname,'public');
 
+app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-const puerto = process.env.PORT || 3000;
+
+app.get('/afip', (req, res) => {
+  res.render('afip');
+});
+
 
 // Inicia el servidor en el puerto especificado
 app.listen(puerto, () => {
