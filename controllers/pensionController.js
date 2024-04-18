4394163c4e5bbed0d6921aa8175565ef33f1d419
Lucas Banegas <131389097+lmbanegas@users.noreply.github.com>
2024-04-18 09:19:42 -0300

    Update pensionController.js

--- controllers/pensionController.js
+++ controllers/pensionController.js
@@ -296,10 +296,26 @@ const pensionController = {
             descuentoCausante = (descuentoCausante / 30) * (diaF) + (descuentoCausante * (diaFA+diaF)/30/12)
             
         } else {
-        brutoCausante = 10000;
-        descuentoCausante = 9900;
-
-        console.log(brutoCausante);
+            
+            if (req.body.mesF == 2 || req.body.mesF == 8) {
+              diaFA = 30;
+            } 
+            
+            if (req.body.mesF == 3 || req.body.mesF == 9) {
+              diaFA = 60;
+            } 
+            
+             if (req.body.mesF == 4 || req.body.mesF == 10) {
+              diaFA = 90;
+            } 
+            
+            if (req.body.mesF == 5 || req.body.mesF == 11) {
+              diaFA = 120;
+            }
+        
+            brutoCausante =  (brutoCausante * ((diaFA+diaF)/30/12))
+            descuentoCausante = (descuentoCausante * (diaFA+diaF)/30/12)
+            
             
         }
         
