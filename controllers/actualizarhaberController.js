e9a47f8ccf7acded21f8bf0aa0b4d2b429eed20b
Lucas Banegas <131389097+lmbanegas@users.noreply.github.com>
2024-05-21 09:10:37 -0300

    Update actualizarhaberController.js

--- controllers/actualizarhaberController.js
+++ controllers/actualizarhaberController.js
@@ -130,7 +130,7 @@ const actualizarhaberController = {
             {
                 "desde": { dia: 1, mes: 05, anio: 2024 },
                 "hasta": { dia: 31, mes: 05, anio: 2024 },
-                "proporcionalMeses": 2,
+                "proporcionalMeses": 1,
                 "aumento": 1.1101,
                 "minima": 190141.60,
                 "os": 5704.25,
