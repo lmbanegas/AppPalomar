36cd1e76a10f83c5e406c05202d9e33e5f17f875
Lucas Banegas <131389097+lmbanegas@users.noreply.github.com>
2024-06-11 09:30:22 -0300

    Update actualizarhaberController.js

--- controllers/actualizarhaberController.js
+++ controllers/actualizarhaberController.js
@@ -198,7 +198,7 @@ const actualizarhaberController = {
         //Formulas para crear nuevo array que comience desde la Fecha Inicial de Pago
 const indiceInicio = meses.findIndex(mes => {
 
-    if (fip[0].mes < 4 && fip[0].anio == 2024) {
+    if (fip[0].mes > 4 && fip[0].anio == 2024) {
         if (fip[0].mes === 5 || fip[0].mes === 4) {
             fip[0].mes = 3;
         } else if (fip[0].mes === 7 || fip[0].mes === 8) {
